1966a7e0794c278a5a329655eda51c918916bc1e
Aakarshan Jorge <aaka022009@gmail.com>
2025-08-11 01:22:21 +0200

    Added the API and recieve a positive response

--- triviaGO/src/App.jsx
+++ triviaGO/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useState, useEffect } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
+  
+  const API_BASE_URL = "https://opentdb.com/api.php?amount=10";
+
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    }
+  }
+
+  const fetchQuestions = async () => {
+    try{  
+      const SESSION_TOKEN_LINK = "https://opentdb.com/api_token.php?command=request";
+
+      const sessionToken = await fetch(SESSION_TOKEN_LINK, API_OPTIONS);
+
+      if(!sessionToken.ok){
+        throw new Error("Failed fetching Session Token");
+      }
+
+      const sessionTokenData = await sessionToken.json();
+
+      const endpoint = `${API_BASE_URL}&token=${sessionTokenData.token}&difficulty=easy`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error("Failed fetching questions");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    }
+    catch(error){
+      console.error(`Failed to fetch questions: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    fetchQuestions();
+  }, [])
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Question</h1>
+      <p>List</p>
+    </div>
   )
 }
 
